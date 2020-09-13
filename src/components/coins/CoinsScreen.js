da79b6e1cac9ef8757f8897a85b7636014ef3d35
David Barcenas <daveepro@outlook.com>
2020-09-13 14:52:40 -0500

    Navigate to CoinDetailScreen

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import React from 'react'
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
-export class CoinsScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Coins Sreen</Text>
-      </View>
-    );
-  }
+export const CoinsScreen = ({navigation: { navigate }}) => {
+
+  const handlePress = () => navigate('CoinDetail')
+
+  return (
+    <View style={ styles.container }>
+      <Text>Coins Sreen</Text>
+      <Pressable style={ styles.btn } onPress={ handlePress }>
+        <Text style={ styles.btnText }>go to detail</Text>
+      </Pressable>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
+  btn: {
+    padding: 0,
+    backgroundColor: 'blue'
+  },
+  btnText: {
+    color: '#fff'
+  }
+})
