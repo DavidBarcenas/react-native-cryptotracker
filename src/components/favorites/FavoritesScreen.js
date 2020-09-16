453b9dc8f1e5ea17a6077ddcd0d285f1c54a7124
David Barcenas <daveepro@outlook.com>
2020-09-16 13:05:08 -0500

    Create favorites list design

--- src/components/favorites/FavoritesScreen.js
+++ src/components/favorites/FavoritesScreen.js
@@ -1,11 +1,43 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { FlatList, StyleSheet, View } from 'react-native'
+import { StorageLib } from '../../libs/storage'
+import { CoinsItem } from '../coins/CoinsItem'
 import { FavoritesEmptyState } from './FavoritesEmptyState'
 
-export const FavoritesScreen = () => {
+export const FavoritesScreen = ({ navigation }) => {
+  const [favorites, setFavorites] = useState([])
+
+  getFavorites = async () => {
+    try {
+      const allKeys = await StorageLib.instance.getAllKeys()
+      const keys = allKeys.filter((key) => key.includes('favorite-'));
+      const favs = await StorageLib.instance.multiGet(keys) 
+      const favorites = favs.map((fav) => JSON.parse(fav[1]));
+      setFavorites(favorites)
+    } 
+    catch (error) {
+      console.log('get favorites error', error)
+    }
+  }
+
+  const handlePress = (coin) => {
+    navigation.navigate('CoinDetail', { coin })
+  }
+
+
+  useEffect(() => {
+    navigation.addListener('focus', getFavorites)
+    return () => {
+      navigation.removeListener('focus', getFavorites)
+    }
+  }, [])
+
   return (
     <View style={ style.container }>
-      <FavoritesEmptyState />
+      { favorites.length == 0 ? 
+        <FavoritesEmptyState /> :  
+        <FlatList data={ favorites } renderItem={({ item }) => <CoinsItem item={ item } onPress={ () => handlePress(item) } />} />
+      }
     </View>
   )
 }
